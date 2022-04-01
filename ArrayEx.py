55619e751625b15d5187654c37ef57d942fb35f5
lakshmiyagnanandareddy <mudhireddynandu@gmail.com>
2022-04-01 23:51:14 +0530

    After corrections

--- ArrayEx.py
+++ ArrayEx.py
@@ -1,115 +1,101 @@
 import array as arr
 
 
-def ex():
-    a = arr.array("i", [1, 4, 4, 7, 8, 8])
-    k = range(len(a))
-    for i in k:
-        x = 0
-        for j in k:
-            if a[i] == a[j]:
-                x = x+1
-                k = k-1
-                y = a[j]
-                a.remove(y)
-        print(a[i], "is occurred in an array is", x, "times")
-
-
-#  removing duplicate
 def main():
 
-    def ex(k, a, j):
-        if j == k:
-            return a
-        elif j < k:
-            for i in range(k):
-                if i == j:
+    def removes_duplicate_elements_in_array(array_list, array_list_index):
+        if array_list_index == len(array_list):
+            return array_list
+        elif array_list_index < len(array_list):
+            for i in range(len(array_list)):
+                if i == array_list_index:
                     pass
-                elif a[i] == a[j]:
-                    x = a[i]
-                    a.remove(x)
-                    k = k-1
-                    return ex(k, a, j)
-            return ex(k, a, j + 1)
+                elif array_list[i] == array_list[array_list_index]:
+                    removing_element = array_list[i]
+                    array_list.remove(removing_element)
+                    return removes_duplicate_elements_in_array(array_list, array_list_index)
 
-    a = arr.array("i", [1, 2, 1, 3, 4, 4, 7, 5])
-    j = 0
-    k = len(a)
-    reverse = ex(k, a, j)
-    print(reverse)
+            return removes_duplicate_elements_in_array(array_list, array_list_index + 1)
 
-# occurence
-def occur():
-
-    def occ(a, j, k, b):
-        if b <= j-1:
-            i = 0
-            x = 0
-            for i in range(j):
-                if a[i] == a[b]:
-                    x = x+1
-            print(a[b], "is occurred :", x, "times")
-            return occ(a, j, k, b+1)
-    a = arr.array("i", [1, 3, 4, 6, 3, 1, 1])
-    j = len(a)
-    k = 0
-    b = 0
-    occ(a, j, k, b)
+    array_list = arr.array("i", [1, 2, 1, 3, 4, 4, 7, 5])
+    array_list_index = 0
+    array_list = removes_duplicate_elements_in_array(array_list, array_list_index)
+    print(array_list)
 
 
-def remov():
+def find_number_of_occurrence():
+
+    def find_number_of_occurrence(array_list, array_list_index):
+        if array_list_index <= len(array_list)-1:
+            num_of_occurrence = 0
+            for i in range(len(array_list)):
+                if array_list[i] == array_list[array_list_index]:
+                    num_of_occurrence = num_of_occurrence+1
+            print(array_list[array_list_index], "is occurred :", num_of_occurrence, "times")
+
+            return find_number_of_occurrence(array_list, array_list_index + 1)
+
+    array_list = arr.array("i", [1, 3, 4, 6, 3, 1, 1])
+    array_list_index = 0
+    find_number_of_occurrence(array_list, array_list_index)
+
+
+def remove():
     import array as arr
-    a = arr.array("i", [1, 2, 3, 4, 5, 3, 6])
-    a.remove(3)
-    print(a)
+    array_list = arr.array("i", [1, 2, 3, 4, 5, 3, 6])
+    array_list.remove(3)
+    print(array_list)
 
 
-def duplic():
-        def ex(k, a, j):
-            if j == k:
-                return True
-            elif j < k:
-                for i in range(k):
-                    if i == j:
-                        pass
-                    elif a[i] == a[j]:
-                        return False
-                return ex(k, a, j + 1)
+def find_list_contains_any_duplicate_element():
 
-        a = arr.array("i", [1, 2, 3, 4, 5])
-        j = 0
-        k = len(a)
-        print((ex(k, a, j)))
+    def find_list_contain_any_duplicate_element(array_list, array_list_index):
+        if array_list_index == len(array_list):
+            return True
+        elif array_list_index < len(array_list):
+            for i in range(len(array_list)):
+                if i == array_list_index:
+                    pass
+                elif array_list[i] == array_list[array_list_index]:
+                    return False
+
+            return find_list_contain_any_duplicate_element(array_list, array_list_index + 1)
+
+        array_list = arr.array("i", [1, 2, 3, 4, 5])
+        array_list_index = 0
+        print((find_list_contain_any_duplicate_element(array_list, array_list_index)))
 
 
-def firstOccur():
-    def ex(k, a, j):
-        if j == k:
+def find_first_repeating_number():
+
+    def find_first_repeating_number(array_list, array_list_index):
+        if array_list_index == (len(array_list)):
             return -1
-        elif j < k:
-            for i in range(k):
-                if i == j:
+
+        elif array_list_index < len(array_list):
+            for i in range(len(array_list)):
+                if i == array_list_index:
                     pass
-                elif a[i] == a[j]:
-                    return a[i], "is the first occurence value"
-            return ex(k, a, j + 1)
+                elif array_list[i] == array_list[array_list_index]:
+                    return array_list[i], "is the first repeating value"
 
-    a = arr.array("i", [1, 2, 3, 4, 5, 5])
-    j = 0
-    k = len(a)
-    print((ex(k, a, j)))
+            return find_first_repeating_number(array_list, array_list_index + 1)
+
+    array_list = arr.array("i", [1, 2, 3, 4, 5])
+    array_list_index = 0
+    print((find_first_repeating_number(array_list, array_list_index)))
 
 
-def FindingSequence():
+def finding_sequence():
     import array as arr
-    a = arr.array("i", [10, 14, 15, 16, 18, 20])
-    j = 0
-    for k in range(min(a), max(a)):
-        if k == a[j]:
-            j = j+1
+    array_list = arr.array("i", [10, 14, 15, 16, 18, 20])
+    array_list_index = 0
+    for missing_number in range(min(array_list), max(array_list)):
+        if missing_number == array_list[array_list_index]:
+            array_list_index = array_list_index+1
             pass
         else:
-            print(k)
+            print(missing_number, "", end="")
 
 
 def ad():
@@ -130,5 +116,12 @@ def ad():
         print(array_num[i], end=' ')
     print("\nMissing number in the said array (10-20): ", test(array_num))
 
+def mama():
+    def tips_none(lst, fn=lambda x: x):
+      return all(not fn(x) for x in lst)
 
-ad()
+    print(tips_none([0, 1, 2, 0], lambda x: x >= 2 ))
+    print(tips_none([0, 0, 0]))
+
+
+finding_sequence()
